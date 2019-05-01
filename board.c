26747bd3c46005b702628e71a8dda1293b80a530
J <jorgespaguettini@gmail.com>
2019-05-01 23:49:20 +0200

    Substitute pow -> ALL

--- board.c
+++ board.c
@@ -20,7 +20,7 @@ Board blankBoard(void) {
     for (int i = 0; i < HEIGHT; i++) {
         for (int j = 0; j < WIDTH; j++) {
             board->values[i][j] = 0;
-            board->possible[i][j] = (int) pow(2, RANGE) - 1;
+            board->possible[i][j] = ALL;
         }
     }
     

93cd1132fed61efeded4b88e7111bacaa48c8225
tempate <ddiazq28@gmail.com>
2020-04-15 18:03:23 +0200

    Added new tests for different sizes

--- tests/test_solver.cpp
+++ tests/test_solver.cpp
@@ -10,7 +10,12 @@
 #include "../src/solver.h"
 #include "main.h"
 
-std::vector<StandardBoard> parse_file(const std::string filename) {
+std::string genFilename() {
+    const std::string size = std::to_string(RANGE);
+    return "../tests/sudokus/" + size + "x" + size + ".txt";
+}
+
+std::vector<StandardBoard> parseFile(const std::string filename) {
     std::ifstream infile(filename);
     std::vector<StandardBoard> boards;
 
@@ -21,8 +26,18 @@ std::vector<StandardBoard> parse_file(const std::string filename) {
         int i = 0;
 
         for (int y = 0; y < HEIGHT; y++) {
-            for (int x = 0; x < WIDTH; x++, i++)
-                board.setValue(Token{x, y}, (line[i] == '.') ? 0 : line[i] - '0');
+            for (int x = 0; x < WIDTH; x++, i++) {
+                int value = 0;
+                
+                if (line[i] >= '1' && line[i] <= '9')
+                    value = line[i] - '0';
+                else if (line[i] >= 'A' && line[i] <= 'Z')
+                    value = line[i] - 'A' + 10;
+                else if (line[i] >= 'a' && line[i] <= 'z')
+                    value = line[i] - 'a' + 10;
+                
+                board.setValue(Token{x, y}, value);
+            }
         }
 
         boards.push_back(board);
@@ -32,10 +47,12 @@ std::vector<StandardBoard> parse_file(const std::string filename) {
 }
 
 void test_solver() {
-    const auto boards = parse_file("../tests/sudokus.txt");
+    const auto boards = parseFile(genFilename());
 
     for (auto board : boards) {
+        //std::cout << board;
         solve<StandardBoard>(board, 1);
+        //std::cout << board;
         assert(board.solved());
     }
 
